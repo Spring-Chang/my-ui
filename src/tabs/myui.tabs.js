8bb3fbf87676e788baec28692ea1c3e8633c3688
Spring-Chang <290794272@qq.com>
2020-04-20 00:12:09 +0800

    创建tabs页面组件功能

---
+++ src/tabs/myui.tabs.js
@@ -0,0 +1,23 @@
+/**
+ * MyUI for jQuery
+ *
+ * Copyright (c) 2020 张翠山
+ *
+ * MIT License
+ *
+ * 联系作者：290794272@qq.com(张翠山)
+ *
+ * tabs组件 - MyUI for jQuery
+ *
+ * 版本：0.0.1
+ */
+
+(function($) {
+
+    // plugin definition
+    $.fn.tabs = function(options) {
+
+    };
+
+    //  ...
+})(jQuery);
